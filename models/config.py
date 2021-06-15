f8f24966aa8eada576741fba5261de194e1fbaa2
cfrazier1978 <66221343+cfrazier1978@users.noreply.github.com>
2021-06-15 10:48:01 -0400

    Initial commit

---
+++ models/config.py
@@ -0,0 +1,42 @@
+from cadCAD.configuration.utils import config_sim
+from cadCAD.configuration import Experiment
+from models.state_variables import genesis_states_1, genesis_states_2
+from models.psubs import partial_state_update_blocks
+
+simulation_parameters = {
+    'T': range(10),
+    'N': 5,
+    # 'M': {}
+}
+
+
+exp = Experiment()    # this is the creation of the experiment object that must appear in the Labs.py
+c = config_sim(simulation_parameters)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# The configurations above are then packaged into a `Configuration` object
+# #.append_model    <-- append model is the new way to do this in the future, append config is the old way.
+exp.append_configs(   
+    model_id = 'sys_model',
+    initial_state=genesis_states_1, #dict containing variable names and initial values
+    partial_state_update_blocks=partial_state_update_blocks, #dict containing state update functions
+    sim_configs=c #preprocessed dictionaries containing simulation parameters
+)
+
+simulation_parameters = {
+    'T': range(10),
+    'N': 5,
+    # 'M': {}
+}
+
+
+exp = Experiment()    # this is the creation of the experiment object that must appear in the Labs.py
+c = config_sim(simulation_parameters)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# The configurations above are then packaged into a `Configuration` object
+# #.append_model    <-- append model is the new way to do this in the future, append config is the old way.
+exp.append_configs(   
+    model_id = 'sys_model',
+    initial_state=genesis_states_2, #dict containing variable names and initial values
+    partial_state_update_blocks=partial_state_update_blocks, #dict containing state update functions
+    sim_configs=c #preprocessed dictionaries containing simulation parameters
+)
---
+++ models/state_variables.py
@@ -0,0 +1,9 @@
+genesis_states_1 = {
+    'box_A': 10, 
+    'box_B': 0 
+}
+
+genesis_states_2 = {
+    'box_A': 10, 
+    'box_B': 4 
+}
